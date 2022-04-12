c0bc3cf56f2e75ec65f26bac02d60a0ee7dea6aa
djbostic <djbostic@ucdavis.edu>
2022-04-11 18:38:48 -0700

    updated code to accomodate 2022 basins

--- step1_compile_minthresholds.R
+++ step1_compile_minthresholds.R
@@ -2,83 +2,28 @@
 library(tidyverse) # general purpose data science toolkit
 library(sp)        # spatial objects
 library(raster)    # for raster objects
+library(here)
+library(sf)
 
+here()
 # set working directory
-#setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-setwd("/Volumes/GoogleDrive/My Drive/Graduate School/GSP_Analy/Organized_CnD/")
+setwd(here())
 
 # set coordinate reference system
 merc <- crs("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0
             +k=1.0 +units=m +nadgrids=@null +no_defs")
 
 # load data
-gsps <- st_read("Boundaries/all_cobs/All_CO_GSPs.shp")
+gsps <- st_read(here("Boundaries/GSP_submitted/GSA_Master.shp"))
 gsps <- st_transform(gsps, merc)
 
-# csv names
-monitoring_network <- list.files("1Compile_MTs/Data/MTs/", pattern = "\\.shp$")
-
-# remove non-CV GSPs - Borrego, Cuyama, Santa Cruz, PasoRobles, Salinas, Oxnard, Pleasant Valley, Indian Wells
-#monitoring_network <- monitoring_network[-c(3, 8, 14:16, 21, 36, 39, 40)]
-
-# remove only Borrego
-monitoring_network <- monitoring_network[-3]
-
-# data list
-mn <- list() 
-
-for(i in 1:length(monitoring_network)){
-  mn[[i]] = st_read(file.path("1Compile_MTs/Data/MTs/", monitoring_network[i]), stringsAsFactors = FALSE)
-}
-
-for(i in 1:length(mn)){
-  mn[[i]]$MT_dtw <- as.numeric(as.character(mn[[i]]$MT_dtw))
-}
-
-for(i in 1:length(mn)){
-  mn[[i]] <- subset(mn[[i]], select = c("Well_ID", "MT_dtw"))
-}
-
-# remove null geometry from Kern River
-#mn[[17]] <- mn[[17]][-36,]
-
-# add column with GSP name
-for(i in 1:length(mn)){
-  mn[[i]]$GSP_Name <- monitoring_network[i]
-  mn[[i]]$GSP_Name <- gsub("_.*","", mn[[i]]$GSP_Name)
-}
-
-mt_merc <- list()
-MTs <- mn
-for(t in 1:length(MTs)){
-  mt_merc[[t]] <- st_transform(MTs[[t]], merc)
-}
-# idk why mt_merc[[4]]'s coordinates are different, but here's a fix
-#mt_merc[[4]] <- st_zm(mt_merc[[4]], drop=TRUE)
+# load MTs
+mn <- read.csv("2020_2022_MTs.csv") %>% filter(is.na(Long)==FALSE & is.na(Lat)==FALSE)
+mts <- st_as_sf(mn, coords = c("Long","Lat"), crs=4326) %>% st_transform(., crs=merc)
 
 # plot to make sure everything looks okay
-plot(gsps$geometry)
-for(t in 1:length(mt_merc)){
-  plot(mt_merc[[t]]$geometry, add=T, cex=.2, col="cornflowerblue") 
-}
+ggplot()+
+  geom_sf(data=gsps, col="blue")+
+  geom_sf(data=mts, col="red")
 
-# combine all MTs into one shapefile
-all_MTs <- do.call(rbind, mt_merc)
-all_MTs <- st_zm(all_MTs, drop=TRUE)
-q <- all_MTs[!st_is_empty(all_MTs),drop=FALSE]
-mt <- as_Spatial(q)
-
-# transform to mercator
-ds <- spTransform(mt, merc)
-
-# output MTs
-#write_rds(ds, "Output/all_mts.rds")
-
-dd <- as(ds, "sf")
-st_write(hey, "1Compile_MTs/Output/MinimumThresholdWells.shp")
-
-hey <- filter(hey, region=="Coastal")
-
-ox <- filter(hey, GSP_Name.1 == "Santa Cruz Mid-County")
-ggplot() +
-  geom_sf(data=hey, aes(col=MT_dtw))
+mt <- as_Spatial(mts)
--- step2_subset_domesticwells.R
+++ step2_subset_domesticwells.R
@@ -3,10 +3,9 @@ library(tidyverse) # general purpose data science toolkit
 library(sf)        # spatial objects
 library(raster)    # for raster objects
 library(readr)
+library(here)
+library(lubridate)
 
-# set working directory
-#setwd(dirname(rstudioapi::getSourceEditorContext()$path))
-setwd("/Volumes/GoogleDrive/My Drive/Graduate School/GSP_Analy/Organized_CnD/")
 
 # set coordinate reference system
 merc <- crs("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0
@@ -14,141 +13,62 @@ merc <- crs("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y
 
 #### load data ####
 # all gsp boundaries
-gsps <- st_read("Boundaries/all_cobs/All_CO_GSPs.shp") %>% st_transform(., merc) %>% filter(., GSP_Name != "Las Posas")
+gsps <- st_read("Boundaries/GSP_submitted/GSA_Master.shp") %>% st_transform(., merc)
 
 # add dictionary of coastal, CV, and southern basins
-gsps$region <- ifelse(gsps$GSP_Name == "Alpaugh" | 
-                      gsps$GSP_Name == "Central Kings" | 
-                      gsps$GSP_Name == "Chowchilla" | 
-                      gsps$GSP_Name == "Fresno County - Delta Mendota" | 
-                      gsps$GSP_Name == "Delano-Earlimart" | 
-                      gsps$GSP_Name == "East Kaweah" | 
-                      gsps$GSP_Name == "Eastern San Joaquin" | 
-                      gsps$GSP_Name == "Eastern Tule" | 
-                      gsps$GSP_Name == "Farmers WD" | 
-                      gsps$GSP_Name == "Greater Kaweah" | 
-                      gsps$GSP_Name == "Northern Central DM" | 
-                      gsps$GSP_Name == "Kern Groundwater Authority" | 
-                      gsps$GSP_Name == "Grasslands" |
-                      gsps$GSP_Name == "Henry Miller" |
-                      gsps$GSP_Name == "James" |  
-                      gsps$GSP_Name == "Kern River" |
-                      gsps$GSP_Name == "Kings River East" |
-                      gsps$GSP_Name == "Lower Tule River ID" |
-                      gsps$GSP_Name == "Mid-Kaweah" |
-                      gsps$GSP_Name == "Madera" |
-                      gsps$GSP_Name == "McMullin Area" |
-                      gsps$GSP_Name == "Merced" |
-                      gsps$GSP_Name == "North Fork Kings" |
-                      gsps$GSP_Name == "New Stone" |
-                      gsps$GSP_Name == "North Kings" |
-                      gsps$GSP_Name == "Olcese" |
-                      gsps$GSP_Name == "Pixley ID" |
-                      gsps$GSP_Name == "Root Creek" |
-                      gsps$GSP_Name == "SJREC" |  
-                      gsps$GSP_Name == "South Kings" |
-                      gsps$GSP_Name == "TriCounty WA - Tule" |
-                      gsps$GSP_Name == "Tulare" |
-                      gsps$GSP_Name == "Westlands" |
-                      gsps$GSP_Name == "Buena Vista" |
-                      gsps$GSP_Name == "Aliso WD", "San Joaquin Valley", 
-                ifelse(gsps$GSP_Name == "Oxnard" | 
-                       gsps$GSP_Name == "Pleasant Valley" |
-                       gsps$GSP_Name == "180_400ftAq - Salinas" |
-                       gsps$GSP_Name == "Santa Cruz Mid-County", "Coastal", 
-                 ifelse(gsps$GSP_Name == "Cuyama" | 
-                        gsps$GSP_Name == "Paso Robles" |
-                        gsps$GSP_Name == "Indian Wells" , "Central", "NA")))
+gsps$region <- ifelse(grepl("5-",gsps$BASIN), "Central Valley",
+                      ifelse(grepl("4-", gsps$BASIN), "Ventura",
+                             ifelse(grepl("6-", gsps$BASIN), "Indian Wells",
+                                          ifelse(grepl("3-", gsps$BASIN), "Cuyama",
+                                                 ifelse(grepl("2-", gsps$BASIN), "Napa", "Other")))))
 
 ca <- st_read("Boundaries/ca/CA_State_TIGER2016.shp")
 ca <- st_transform(ca, merc)
 ggplot()+
   geom_sf(data=ca, fill = "transparent") +
   geom_sf(data=gsps, aes(fill = gsps$region)) +
-  scale_fill_manual("Region", values = c("San Joaquin Valley" = "#70AB53", "Coastal" = "#535EAB", "Central" = "#AB6353")) +
+  scale_fill_manual("Region", values = c("Central Valley" = "#70AB53", "Ventura" = "#535EAB", "Cuyama" = "#AB6353", "Indian Wells" = "red", "Napa"="blue", "Other"="gray90")) +
   theme_void(base_size = 16)
-gsps <- st_zm(gsps, drop=TRUE)
-st_write(gsps, "Output/gsps_with_region.shp", delete_dsn =TRUE)
 
-# Central Valley domestic wells with estimated pump depths (from Rich)
-domcv <- read_rds("2Subset_DWs/Data/domcv6_mean_gw_with_beta_GF_CI.rds")
-domcvda <- as(domcv, "sf")
+# download wells from OSWCR
+wells <- read_csv("DomesticWells/oswcr_bulkdatadownload_04112022/OSWCR.csv") 
+wellz <- wells %>% mutate(DATEWORKENDED = as.Date(DATEWORKENDED, "%m/%d/%Y"), year=year(DATEWORKENDED)) %>% filter(year>1960 & is.na(DECIMALLONGITUDE)==FALSE & is.na(DECIMALLATITUDE)==FALSE & !grepl("/", DECIMALLATITUDE) & !grepl("/", DECIMALLONGITUDE))
 
-#domcvda <- filter(domcvda, year > 1960 & year < 2017)
+wellz <- st_as_sf(wellz, coords = c("DECIMALLONGITUDE","DECIMALLATITUDE"), crs=4326) %>% st_transform(., crs=merc)
+wells_in_gsps <- st_intersection(wellz, gsps)
+domesticwells_in_gsps <- filter(wells_in_gsps, grepl("domestic", PLANNEDUSEFORMERUSE, ignore.case = TRUE))
 
-# california cleaned domestic wells (from Rich)
-noncvgsp <- filter(gsps, region != "San Joaquin Valley")
+# see how many wells there are through the years
+domsub <- domesticwells_in_gsps %>% filter(year > 1965 & year < 2017)
+length(unique(domsub$WCRNUMBER))
+mean(domsub$TOTALCOMPLETEDDEPTH, na.rm=TRUE)
 
-dom <- st_read("2Subset_DWs/Data/wells/domestic_wells_ca.shp", stringsAsFactors=FALSE) %>% st_transform(., merc)
-dom$year <- as.numeric(dom$year)
-domsub <- dom %>% st_intersection(.,noncvgsp) 
+domsub <- domesticwells_in_gsps %>% filter(year > 1970 & year < 2017)
+length(unique(domsub$WCRNUMBER))
+mean(domsub$TOTALCOMPLETEDDEPTH, na.rm=TRUE)
 
-domcvda <- filter(domcvda, year > 1960 & year < 2017)
-domsub <- domsub %>% filter(year > 1960 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
+domsub <- domesticwells_in_gsps %>% filter(year > 1975 & year < 2017)
+length(unique(domsub$WCRNUMBER))
+mean(domsub$TOTALCOMPLETEDDEPTH, na.rm=TRUE)
 
-domcvda <- filter(domcvda, year > 1965 & year < 2017)
-domsub <- domsub %>% filter(year > 1965 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
+domsub <- domesticwells_in_gsps %>% filter(year > 1980 & year < 2017)
+length(unique(domsub$WCRNUMBER))
+mean(domsub$TOTALCOMPLETEDDEPTH, na.rm=TRUE)
 
-domcvda <- filter(domcvda, year > 1970 & year < 2017)
-domsub <- domsub %>% filter(year > 1970 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
+domsub <- domesticwells_in_gsps %>% filter(year > 1985 & year < 2017)
+length(unique(domsub$WCRNUMBER))
+mean(domsub$TOTALCOMPLETEDDEPTH, na.rm=TRUE)
 
-domcvda <- filter(domcvda, year > 1975 & year < 2017)
-domsub <- domsub %>% filter(year > 1975 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
+domsub90 <- domesticwells_in_gsps %>% filter(year > 1990 & year < 2017)
+length(unique(domsub$WCRNUMBER))
+mean(domsub$TOTALCOMPLETEDDEPTH, na.rm=TRUE)
 
-domcvda <- filter(domcvda, year > 1980 & year < 2017)
-domsub <- domsub %>% filter(year > 1980 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
+domsub <- domesticwells_in_gsps %>% filter(year > 1995 & year < 2017)
+length(unique(domsub$WCRNUMBER))
+mean(domsub$TOTALCOMPLETEDDEPTH, na.rm=TRUE)
 
-domcvda <- filter(domcvda, year > 1985 & year < 2017)
-domsub <- domsub %>% filter(year > 1985 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
+domsub <- domesticwells_in_gsps %>% filter(year > 2000 & year < 2017)
+length(unique(domsub$WCRNUMBER))
+mean(domsub$TOTALCOMPLETEDDEPTH, na.rm=TRUE)
 
-domcvda <- filter(domcvda, year > 1990 & year < 2017)
-domsub90 <- domsub %>% filter(year > 1990 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
-
-domcvda <- filter(domcvda, year > 1995 & year < 2017)
-domsub <- domsub %>% filter(year > 1995 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
-
-domcvda <- filter(domcvda, year > 2000 & year < 2017)
-domsub <- domsub %>% filter(year > 2000 & year < 2017)
-length(unique(domcvda$WCRNumber)) + length(unique(domsub$WCRNmbr))
-mean(mean(domcvda$TotalCompletedDepth, na.rm=TRUE), mean(domsub$TtlCmpD, na.rm=TRUE))
-
-#write_rds(domsub, "Output/domesticwells_allcobs_75.rds")
-#write_rds(domsub90, "Output/domesticwells_allcobs_90.rds")
-
-hi <- domsub %>% group_by(GSP_Name) %>% summarise(mean(TtlCmpD, na.rm=TRUE), length(unique(WCRNmbr)))
-
-
-# OSWCR csvs Darcy collected(no estimated pump depths)
-oswcr_files <- list.files("2Subset_DWs/Data/DWs/", pattern = "\\.csv$")
-oswcr <- list()
-for(i in 1:length(oswcr_files)){
-  oswcr[[i]] = read.csv(file.path("2Subset_DWs/Data/DWs/", oswcr_files[i]), stringsAsFactors = FALSE)
-}
-oswcr_shp <- lapply(oswcr, function(x) st_as_sf(x, coords = c("lon", "lat"), crs = "+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs"))
-oswcr_merc <- lapply(oswcr_shp, function(x) st_transform(x, merc))
-oswcr_compiled <- lapply(oswcr_merc, function(x) x %>% dplyr::select(WCRNumber, year, type, TotalCompletedDepth))
-oswcr_compiled <- do.call(rbind, oswcr_compiled)
-
-# filter oswcr data
-oswcr_filtered <- oswcr_compiled %>% filter(year > 1975 & type == "domestic")
-plot(gsps)
-plot(oswcr_filtered$geometry, add=TRUE, col="red")
-
-# export data to be used elsewhere
-write_rds(oswcr_filtered, "Output/oswcr_allcobs.rds")
+hi <- domesticwells_in_gsps %>% group_by(GSP.ID) %>% summarise(mean(TOTALCOMPLETEDDEPTH, na.rm=TRUE), length(unique(WCRNUMBER)))
